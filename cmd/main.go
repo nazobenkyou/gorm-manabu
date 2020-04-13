cf7b27640355d15ea7811957174d5c8ef5a3593a
ervitis <vitomarti@gmail.com>
2020-04-13 16:50:24 +0200

    chore: update doc and fix db conn

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/nazobenkyou/gorm-manabu/config"
 	"github.com/nazobenkyou/gorm-manabu/input/http_router"
+	"github.com/nazobenkyou/gorm-manabu/repository"
 	"net/http"
 )
 
 func main() {
 	config.LoadConfig()
 
-	panic(http.ListenAndServe(config.AppConfig.SrvPort, http_router.NewRouter()))
+	conn, err := repository.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Conn.Close()
+
+	conn.Conn.AutoMigrate(&repository.Task{})
+
+	panic(http.ListenAndServe(config.AppConfig.SrvPort, http_router.NewRouter(conn)))
 }
